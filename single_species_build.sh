4dee6930be6fd4126fc861f59fb43754dfac8f9b
Scott Cain <scott+git@scottcain.net>
2021-05-07 23:53:43 +0000

    init commit; based on same script in website-jbrowse-gff Certainly not working yet

---
+++ single_species_build.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -e
+
+RELEASE=280
+while getopts r:s:a:k: option
+do
+case "${option}"
+in
+r) 
+  RELEASE=${OPTARG}
+  ;;
+s) 
+  SPECIES=${OPTARG}
+  ;;
+a)
+  AWSACCESS=${OPTARG}
+  ;;
+k)
+  AWSSECRET=${OPTARG}
+  ;;
+esac
+done
+
+if [ -z "$RELEASE" ]
+then
+    RELEASE=${WB_RELEASE}
+fi
+
+if [ -z "$SPECIES" ]
+then
+    SPECIES=${WB_SPECIES}
+fi
+
+if [ -z "$AWSACCESS" ]
+then
+    AWSACCESS=${AWS_ACCESS_KEY}
+fi
+
+if [ -z "$AWSSECRET" ]
+then
+    AWSSECRET=${AWS_SECRET_KEY}
+fi
+
+if [ -z "$AWSBUCKET" ]
+then
+    if [ -z "${AWS_S3_BUCKET}" ]
+    then
+        AWSBUCKET=agrjbrowse
+    else
+        AWSBUCKET=${AWS_S3_BUCKET}
+    fi
+fi
+
+MAKEPATH=/website-genome-browsers/protein_schematic/bin/make_schematic.pl
+
+CONFPATH=/website-genome-browsers/protein_schematic/bin/protein_build.conf
+
+LOGFILE=$SPECIES
+LOGFILE+=".log"
+
+#this is by far the longest running portion of the script (typically a few hours)
+$MAKEPATH --conf $CONFPATH --quiet --release $RELEASE --species $SPECIES 2>1 | grep -v "Deep recursion"; mv 1 $LOGFILE
+
+DATADIR=/jbrowse/data
+
+cd $DATADIR
+
+UPLOADTOS3PATH=/agr_jbrowse_config/scripts/upload_to_S3.pl
+
+# this path will need to be fixed for "real" releases. Something like:
+#  REMOTEPATH="MOD-jbrowses/WormBase/WS$RELEASE/$SPECIES"
+REMOTEPATH="test/WS$RELEASE/protein/$SPECIES"
+
+$UPLOADTOS3PATH --bucket $AWSBUCKET --local "$SPECIES/" --remote $REMOTEPATH --AWSACCESS $AWSACCESS --AWSSECRET $AWSSECRET
+ 
+
+
